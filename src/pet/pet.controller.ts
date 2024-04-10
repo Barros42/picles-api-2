c610ba8e27661aa344d3b037dd41d0fc7f29185d
Barros42 <mdbf42@gmail.com>
2024-04-09 22:23:10 -0300

    Feat: Get Pet By Id Flow

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import CreatePetControllerInput from './dtos/create.pet.controller.input';
 import PetTokens from './pet.tokens';
 import { IUseCase } from 'src/domain/iusecase.interface';
@@ -24,8 +24,12 @@ export class PetController {
 
     @Get(':id')
     async getPetById(@Param('id') id: string): Promise<GetPetByIdUseCaseOutput> {
-        const useCaseInput = new GetPetByIdUseCaseInput({ id })
-        return await this.getPetByIdUseCase.run(useCaseInput)
+        try {
+            const useCaseInput = new GetPetByIdUseCaseInput({ id })
+            return await this.getPetByIdUseCase.run(useCaseInput)
+        } catch (error) {
+            throw new BadRequestException(JSON.parse(error.message))
+        }
     }
 
 }
